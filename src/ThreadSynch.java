c7b09703ce23e012a4f061bad9182ce5994d6665
Spencer Yue <spenceryue@gmail.com>
2018-02-12 13:40:22 -0600

    ThreadSynch works i think

--- src/ThreadSynch.java
+++ src/ThreadSynch.java
@@ -1,31 +1,37 @@
 /*
- * EID's of group members
- * 
+ * Kyle Bradford, Spencer Yue
+ * kmb3534, sty223
  */
-import java.util.ArrayDeque;
+
 import java.util.concurrent.Semaphore; // for implementation using Semaphores
 
 public class ThreadSynch {
-    int curIndex;
-    private Semaphore s;
+    private Semaphore barrier, mutex;
+    int active, N;
 
     public ThreadSynch(int parties) {
-        curIndex = parties - 1;
-        s = new Semaphore(1);
+        barrier = new Semaphore(0);
+        mutex = new Semaphore (1);
+        this.active = active;
+        this.N = parties;
     }
 
     public int await() throws InterruptedException {
-        int index = 0;
+        mutex.acquire ();
+        active--;
+        int index = active;
+        mutex.release ();
 
-        // you need to write this code
-        s.acquire();
-        index = curIndex;
-        curIndex--;
-        s.release();
-
-        while(curIndex > 0){
-            wait();
+        if (index > 0)
+          barrier.acquire ();
+        else
+        {
+          barrier.release (N-1);
+          mutex.acquire ();
+          active += N;
+          mutex.release ();
         }
-        return index;    
+
+        return index;
     }
 }
